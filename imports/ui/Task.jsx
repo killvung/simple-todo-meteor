a07c1ec0ba0dd5d5dedfdce285f88fa4866e5e7a
killvung <kiulam125@gmail.com>
2017-02-07 23:42:02 -0600

    add react components - App and Task

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+//Task component, represent a task
+export default class Task extends Component{	
+	render(){
+		return (
+			<li>{this.props.task._id} {this.props.task.text}</li>
+		);
+	}
+}
+
+
+Task.propTypes = {
+	// This component gets the task to display through a React prop.
+	// We can use propTypes to indicate it is required
+	task: PropTypes.object.isRequired,
+}
