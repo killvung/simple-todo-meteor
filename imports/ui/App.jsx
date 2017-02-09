998137b428961e37ce31f46ec55e2c310817abe2
killvung <kiulam125@gmail.com>
2017-02-08 20:32:05 -0600

    Convert fetching data from mongo

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
 //App component - represents the whole app
-export default class App extends Component {
-	getTasks(){
-		return [
-			{ _id: 1, text: 'Task 1'},
-			{ _id: 2, text: 'Task 2'},
-			{ _id: 3, text: 'Task 3'}
-		];
-	}
+class App extends Component {
+
+	//Use MongoDB to get the date instead
+	// getTasks(){
+	// 	return [
+	// 		{ _id: 1, text: 'Task 1'},
+	// 		{ _id: 2, text: 'Task 2'},
+	// 		{ _id: 3, text: 'Task 3'}
+	// 	];
+	// }
 
 	renderTasks(){
-		return this.getTasks().map((task) => (			
+		return this.props.tasks.map((task) => (			
 			<Task key={task._id} task={task} />
 		));
 	}
@@ -32,3 +37,15 @@ export default class App extends Component {
 		);
 	}
 }
+
+App.propTypes = {
+	tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+	return {
+		tasks: Tasks.find({}).fetch(),
+	};
+}, App);
+
+
